66a866377e50e26a3ba92617f96d859f63423c88
marcos.mota <marcos.mota@quintoandar.com.br>
2025-02-04 11:02:39 -0300

    feat: add FeatureStore class for managing feature datasets and testing setup

---
+++ libs/feature_dataset/feature_dataset.py
@@ -0,0 +1,24 @@
+
+
+from typing import Any, List, Optional
+import pandas as pd
+import duckdb
+import os
+
+DEFAULT_FEATURE_STORE_FILE = "data/feature_store.duckdb"
+
+
+class FeatureStore:
+    def __init__(self, database_file: Optional[str]):
+        self._con = duckdb.connect(database=(database_file or DEFAULT_FEATURE_STORE_FILE))
+
+    def get_feature_dataset(self, name: str, features: Optional[List[str]] = None) -> duckdb.DuckDBPyRelation:
+        if features:
+            features_str = ", ".join(features)
+        else:
+            features_str = "*"
+        query = f"SELECT {features_str} FROM fd_{name}_1"
+        return self._con.query(query)
+
+    def create_feature_dataset(self, name: str, dataset: pd.DataFrame, version: int = 1):
+        self._con.execute(f"CREATE OR REPLACE TABLE fd_{name}_{version} AS SELECT * FROM dataset")
