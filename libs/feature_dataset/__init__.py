84fd54be45ef7272c0cea0093b911e8ef7305eb8
marcos.mota <marcos.mota@quintoandar.com.br>
2025-02-04 16:58:28 -0300

    feat: rename FeatureStore to FeatureDataset and update related methods

---
+++ libs/feature_dataset/__init__.py
@@ -0,0 +1,3 @@
+from .feature_dataset import FeatureDataset
+
+__ALL__ = [FeatureDataset]
--- tests/libs/feature_store/test_feature_store.py
+++ tests/libs/feature_store/test_feature_store.py
@@ -1,18 +1,20 @@
 import os
 import pytest
 import pandas as pd
-from feature_dataset.feature_dataset import FeatureStore
+from feature_dataset.feature_dataset import FeatureDataset
 
 TEST_DB_FILE = "test_feature_store.duckdb"
 
+
 @pytest.fixture(scope="module")
 def feature_store():
-    fs = FeatureStore(TEST_DB_FILE)
+    fs = FeatureDataset(TEST_DB_FILE)
     yield fs
     os.remove(TEST_DB_FILE)
 
+
 def test_create_feature_dataset(feature_store):
-    data = {'feature1': [1, 2, 3], 'feature2': [4, 5, 6]}
+    data = {"feature1": [1, 2, 3], "feature2": [4, 5, 6]}
     df = pd.DataFrame(data)
 
     feature_store.create_feature_dataset("test", df)
@@ -21,8 +23,9 @@ def test_create_feature_dataset(feature_store):
 
     pd.testing.assert_frame_equal(df, result_df)
 
+
 def test_get_feature_dataset_with_specific_features(feature_store):
-    data = {'feature1': [1, 2, 3], 'feature2': [4, 5, 6]}
+    data = {"feature1": [1, 2, 3], "feature2": [4, 5, 6]}
     df = pd.DataFrame(data)
 
     feature_store.create_feature_dataset("test", df)
