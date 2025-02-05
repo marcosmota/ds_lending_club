eef5687004c1d93634c58d7a75191d7a1bb6b36e
marcos.mota <marcos.mota@quintoandar.com.br>
2025-02-05 09:28:01 -0300

    refactor: update FeatureDataset to use a database file attribute and manage connections

--- libs/feature_dataset/feature_dataset.py
+++ libs/feature_dataset/feature_dataset.py
@@ -8,23 +8,26 @@ DEFAULT_FEATURE_STORE_FILE = "data/feature_store.duckdb"
 
 class FeatureDataset:
     def __init__(self, database_file: Optional[str] = None):
-        self._con = duckdb.connect(
-            database=(database_file or DEFAULT_FEATURE_STORE_FILE)
-        )
+        self._database_file = database_file or DEFAULT_FEATURE_STORE_FILE
 
     def get_feature_dataset(
         self, name: str, features: Optional[List[str]] = None
     ) -> duckdb.DuckDBPyRelation:
+        con = duckdb.connect(database=self._database_file)
         if features:
             features_str = ", ".join(features)
         else:
             features_str = "*"
         query = f"SELECT {features_str} FROM fd_{name}_1"
-        return self._con.query(query)
+        data = con.query(query).to_df()
+        con.close()
+        return data
 
     def create_feature_dataset(
         self, name: str, dataset: pd.DataFrame, version: int = 1
     ):
-        self._con.execute(
+        con = duckdb.connect(database=self._database_file)
+        con.execute(
             f"CREATE OR REPLACE TABLE fd_{name}_{version} AS SELECT * FROM dataset"
         )
+        con.close()
